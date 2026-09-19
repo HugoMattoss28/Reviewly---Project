e02c21b861466cfe58bf7b280bac699bd738dd34
hugo mattos <hugomattos2810@gmail.com>
2026-09-18 21:38:26 -0300

    Atualizando model

--- src/java/model/Serie.java
+++ src/java/model/Serie.java
@@ -1,19 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Aluno
- */
-public class Model {
+public class Serie {
+    
+    // 1. Atributos (Estado do objeto)
     private int id;
     private String titulo;
     private String genero;
     private int anoLancamento;
-    private Double nota;
-    
-    
+    private double nota;
+
+    // 2. Construtor Vazio
+    public Serie() {
+    }
+
+    // 3. Construtor com Parâmetros
+    public Serie(int id, String titulo, String genero, int anoLancamento, double nota) {
+        this.id = id;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+        this.nota = nota;
+    }
+
+    // 4. Métodos Getters e Setters (Comportamento de acesso)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
 }
