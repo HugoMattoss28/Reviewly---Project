354e873c6101ae05874103fccbdcacc990b29193
hugo mattos <hugomattos2810@gmail.com>
2026-09-18 21:32:06 -0300

    Criando a model

---
+++ src/java/model/Serie.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Model {
+    private int id;
+    private String titulo;
+    private String genero;
+    private int anoLancamento;
+    private Double nota;
+    
+    
+}
